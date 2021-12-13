508a02d5e614daaa6324e690471546da1c236dd6
Claire Marconi <claire.marconi94@gmail.com>
2021-12-13 18:02:44 +0100

    added scooters action

--- app/controllers/scooters_controller.rb
+++ app/controllers/scooters_controller.rb
@@ -1,12 +1,26 @@
 class ScootersController < ApplicationController
   # before_action set_scooter only: [--,--,--]
   def index
+    @scooters = Scooter.all
+  end
+
+  def show
+    @scooter = Scooter.find(params[:id])
   end
 
   def new
+    @scooter = Scooter.new
   end
 
   def create
+    @scooter = Scooter.new(scooter_params)
+    @user = current_user
+    @scooter.user = @user
+    if @scooter.save!
+      redirect_to scooters_path
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -24,5 +38,6 @@ class ScootersController < ApplicationController
   end
 
   def scooter_params
+    params.require(:scooter).permit(:model, :description)
   end
 end
