a1120a8d89988ffb1b93207f894f1c33dfd6b164
adamzermane <adamzermane92@yahoo.com>
2021-12-13 17:49:59 +0100

    bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,18 @@
 class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(param[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  
+
+
 end
