563d81332df9ba2f164e45bd07abac33f1dc6302
Claire Marconi <claire.marconi94@gmail.com>
2021-12-14 17:24:07 +0100

    split bookings according to scooter.user

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,14 @@ class DashboardController < ApplicationController
   def profile
     @user = current_user
     @bookings = @user.bookings
+    @bookings_demands = []
+    @bookings_requests = []
+    @bookings.each do |booking|
+      if booking.scooter.user == @user
+        @bookings_demands << booking
+      else
+        @bookings_requests << booking
+      end
+    end
   end
 end
