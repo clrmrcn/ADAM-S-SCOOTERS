10e811ea50359c5003851cfccead6df9b6fdbaab
adamzermane <adamzermane92@yahoo.com>
2021-12-14 17:58:24 +0100

    implemented pundits on the scooter login, with edit and delete between users

--- app/controllers/scooters_controller.rb
+++ app/controllers/scooters_controller.rb
@@ -2,7 +2,7 @@ class ScootersController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    @scooters = Scooter.all
+    @scooters = policy_scope(Scooter)
   end
 
   def show
@@ -11,39 +11,47 @@ class ScootersController < ApplicationController
 
   def new
     @scooter = Scooter.new
+    authorize @scooter
   end
 
   def create
     @scooter = Scooter.new(scooter_params)
     @user = current_user
     @scooter.user = @user
+    authorize @scooter
     if @scooter.save!
       redirect_to scooters_path
     else
       render 'new'
     end
+    authorize @scooter
   end
 
   def edit
     @scooter = Scooter.find(params[:id])
+    authorize @scooter
     @user = current_user
+
+
   end
 
   def update
     @user = current_user
     @scooter = Scooter.find(params[:id])
-    @scooter.update(scooter_params)
-    if @scooter.save
+    authorize @scooter
+    if @scooter.update(scooter_params)
       flash[:success] = "Scooter updated !"
       redirect_to scooters_path
     else
       render action: :edit
     end
+
   end
 
   def destroy
     @user = current_user
     @scooter = Scooter.find(params[:id])
+    authorize @scooter
     @scooter.destroy
     redirect_to scooters_path
   end
